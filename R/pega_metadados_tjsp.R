4f015515da459c526ad98cda03d3bc39443c6b70
adamiani <athos.damiani@gmail.com>
2014-02-27 12:24:46 -0300

    teste de pull request

--- R/pega_metadados_tjsp.R
+++ R/pega_metadados_tjsp.R
@@ -32,7 +32,8 @@ pega_metadados_tjsp <- function(p) {
   if(!k) cat('\n')
   html <- htmlParse(content(r, as='text'), encoding='UTF-8')
   
-  vet_dados <- sapply(getNodeSet(html, "//table[@id != 'secaoFormConsulta' and (@class='secaoFormBody' or @id='tablePartesPrincipais')]//tr//td"), xmlValue)
+  xpath <- "//table[@id != 'secaoFormConsulta' and (@class='secaoFormBody' or @id='tablePartesPrincipais')]//tr//td"
+  vet_dados <- sapply(getNodeSet(html, xpath), xmlValue)
   vet_dados <- str_trim(gsub(' +',' ',gsub('[\n\t\r]','',vet_dados)))
   vet_dados <- vet_dados[!duplicated(vet_dados,incomparables="")]
   # print(vet_dados)
