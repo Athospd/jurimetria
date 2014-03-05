4c74f6c29bfc4fa30d270edd13f3c06fbe740327
Julio Trecenti <julio@Trecenti.(none)>
2014-03-04 23:33:30 -0300

    improvings and error handling

--- R/crawler_cjpg.R
+++ R/crawler_cjpg.R
@@ -4,41 +4,57 @@
 #' informações que o TJSP precisa para retornar as informações.
 #' 
 #' @export
-crawler_cjpg <- function(livre='', classes='', assuntos='', magistrados='', datas='', varas='') {
-  query <- list(
-    dadosConsulta.pesquisaLivre = livre,
-    tipoNumero = 'UNIFICADO',
-    numeroDigitoAnoUnificado = '',
-    foroNumeroUnificado = '',
-    dadosConsulta.nuProcesso = '',
-    dadosConsulta.nuProcessoAntigo = '',
-    classeTreeSelection.values = classes,
-    classeTreeSelection.text = '',
-    assuntoTreeSelection.values = assuntos,
-    assuntoTreeSelection.text = '',
-    agenteSelectedEntitiesList = '',
-    contadoragente = '0',
-    contadorMaioragente = '0',
-    cdAgente = magistrados,
-    nmAgente = '',
-    dadosConsulta.dtInicio = '',
-    dadosConsulta.dtFim = '',
-    varasTreeSelection.values = datas,
-    varasTreeSelection.text = '',
-    dadosConsulta.ordenacao = 'DESC'
-  )
+bug <- c()
+crawler_cjpg <- function(livre='', classes='', assuntos='', magistrados='', datas=c('',''), varas='', pag=0, r=NULL) {
+  if(is.null(r)) {
+    query <- list(
+      dadosConsulta.pesquisaLivre = livre,
+      tipoNumero = 'UNIFICADO',
+      numeroDigitoAnoUnificado = '',
+      foroNumeroUnificado = '',
+      dadosConsulta.nuProcesso = '',
+      dadosConsulta.nuProcessoAntigo = '',
+      classeTreeSelection.values = classes,
+      classeTreeSelection.text = '',
+      assuntoTreeSelection.values = assuntos,
+      assuntoTreeSelection.text = '',
+      agenteSelectedEntitiesList = '',
+      contadoragente = '0',
+      contadorMaioragente = '0',
+      cdAgente = magistrados,
+      nmAgente = '',
+      dadosConsulta.dtInicio = datas[1],
+      dadosConsulta.dtFim = datas[2],
+      varasTreeSelection.values = varas,
+      varasTreeSelection.text = '',
+      dadosConsulta.ordenacao = 'DESC'
+    )
+    urlb <- list(scheme='https', hostname='esaj.tjsp.jus.br', path='cjpg/pesquisar.do', query=query)
+    class(urlb) <- 'url'
+    url <- build_url(urlb)
+    r <- GET(url, config=list(ssl.verifypeer=F))
+  }
   
-  urlb <- list(scheme='https', hostname='esaj.tjsp.jus.br', path='cjpg/pesquisar.do', query=query)
-  class(urlb) <- 'url'
-  url <- build_url(urlb)
-  url_pag <- 'https://esaj.tjsp.jus.br/cjpg/trocarDePagina.do?pagina=0'
+  url_pag <- sprintf('https://esaj.tjsp.jus.br/cjpg/trocarDePagina.do?pagina=%d',pag)
   # REQUESTS
-  r <- GET(url, config=list(ssl.verifypeer=F))
+  cat(sprintf('pagina: %d...', pag))
+
+  cat('fazendo download...')
   r_pag <- GET(url_pag, config=c(ssl.verifypeer=F, set_cookies(unlist(r$cookies))))
-  html <- htmlParse(content(r_pag, 'text'), encoding='UTF-8')
-  nodes <- getNodeSet(html, "//tr[@class='fundocinza1']//table")  
-  df <- ldply(nodes, parse_node)
-  df
+  cat('download realizado! ')
+  cat('inicializando parser...')
+  try ({
+    html <- htmlParse(content(r_pag, 'text'), encoding='UTF-8')
+    nodes <- getNodeSet(html, "//tr[@class='fundocinza1']//table")  
+    cat('rodando parser...')
+    df <- ldply(nodes, parse_node)
+    df$pag <- pag
+    cat('OK!\n')
+    return(df)  
+  },TRUE)
+  cat('BUGOU!!!!!\n')
+  bug <<- append(bug, pag)
+  return(data.frame())
 }
 
 # other functions
@@ -53,12 +69,31 @@ parse_node <- function(node) {
   children <- xmlChildren(node)
   df <- do.call(cbind, lapply(children[2:(length(children)-1)], parse_node_meta))
   df$n_processo <- gsub('[\n\r\t ]', '', xmlValue(xmlChildren(xmlChildren(children[[1]])$td)$a))
-  df$cod_sentenca <- xmlGetAttr(xmlChildren(xmlChildren(a)$td)$a,'name')
+  df$cod_sentenca <- xmlGetAttr(xmlChildren(xmlChildren(children[[1]])$td)$a,'name')
   df$txt <-  gsub('[\r\t]', '',xmlValue(xmlChildren(xmlChildren(children[[length(children)]])$td)[[4]]))
   df
 }
 
-
-
-
-
+# bug <- c(-1)
+# inicio do download realizado em 2014-02-28 8:17
+# aux <- crawler_cjpg(pag=1, datas=c('','27/02/2014'))
+# load('../testes/r.RData')
+# pags <- as.numeric(xmlGetAttr(getNodeSet(htmlParse(content(r,'text'), encoding='UTF-8'), "//a[@title='Última página']")[[1]], 'name'))
+# lista_df <- list()
+# 
+# for(i in 22108:pags) {
+#   if((i-1) %in% bug) {
+#     lista_df[[i]] <- crawler_cjpg(pag=i, datas=c('','27/02/2014'))
+#     load('../testes/r.RData')
+#   } else {
+#     lista_df[[i]] <- crawler_cjpg(pag=i, r=r)
+#   }
+#   if(i%%1000==0) {
+#     cat('\n\nsalvando lista :)\n\n')
+#     save(lista_df, file='../testes/lista_df.RData')
+#   }
+# }
+# cjpg <- rbind.fill(lista_df)
+# save(cjpg, file='../testes/cjpg.RData')
+# cjpg_meta <- select(cjpg, -(txt))
+# save(cjpg_meta, file='../testes/cjpg_meta.RData')
